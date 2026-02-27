9bf132e6b752587f5817b82134d3ab622e783e81
arsalan2006 <arsalanazam507@gmail.com>
2026-02-27 10:17:02 +0530

    Create Print name.java

---
+++ Print name.java
@@ -0,0 +1,10 @@
+public class MyName {
+    void printName() {
+        System.out.println("Arsalan");
+    }
+
+    public static void main(String[] args) {
+        MyName obj = new MyName();
+        obj.printName();
+    }
+}
